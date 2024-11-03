fc9ab07830389e5b04be0572de3d9d4a1cb4f11a
igorsiata <igorsiata@student.agh.edu.pl>
2024-11-03 23:23:49 +0100

    Documentation and type hinting

--- scripts/optimalization/simulated_annealing.py
+++ scripts/optimalization/simulated_annealing.py
@@ -5,37 +5,78 @@ from copy import deepcopy
 
 
 class SimulatedAnnealing:
-    def __init__(self) -> None:
-        self.lights_times = [{Direction.SOUTH: 20,
-                             Direction.WEST: 20,
-                             Direction.NORTH: 20,
-                             Direction.EAST: 20} for _ in range(4)]
-        self.lights_permutation = [[Direction.SOUTH,
-                                   Direction.WEST,
-                                   Direction.NORTH,
-                                   Direction.EAST]for _ in range(4)]
-        self.lights_cycle = self.generate_cycle(self.lights_times,
-                                                self.lights_permutation)
-        self.simulation = Simulation(100, 50)
+    type LightsTimes = List[Dict[Direction, int]]
+    type LightsPermutation = List[List[Direction]]
 
-    def fitness(self, lights_cycle) -> int:
+    def __init__(self) -> None:
+        # To find neighbour easly light cycle can be represented as
+        # list of times for each direction and
+        # list of permutations specifying order of lights
+        # this can be converted to lights_cycle (List[Direction])
+        self.lights_times: \
+            SimulatedAnnealing.LightsTimes = [{Direction.SOUTH: 20,
+                                               Direction.WEST: 20,
+                                               Direction.NORTH: 20,
+                                               Direction.EAST: 20}
+                                              for _ in range(4)]
+        self.lights_permutation: \
+            SimulatedAnnealing.LightsPermutation = [[Direction.SOUTH,
+                                                     Direction.WEST,
+                                                     Direction.NORTH,
+                                                     Direction.EAST]
+                                                    for _ in range(4)]
+        self.lights_cycle: \
+            List[Crossroad.LightsCycle] = self.generate_cycle(self.lights_times,
+                                                              self.lights_permutation)
+        self.simulation = Simulation(100, 20)
+
+    def fitness(self, lights_cycle: List[Crossroad.LightsCycle]) -> int:
+        """
+        Evaluates solution
+
+        Args:
+            lights_cycle (List[Crossroad.LightsCycle]): Solution to evaluate
+
+        Returns:
+            int: score
+        """
         for i, crossroad in enumerate(self.simulation.crossroad_network.crossroad_network):
             crossroad.lights_cycle = lights_cycle[i]
-        return self.simulation.run()
+        return self.simulation.run()/100000
 
-    def neigbour(self):
+    def neigbour(self) -> LightsTimes:
+        """
+        Finds random solution in neighbourhood of current one
+
+        Returns:
+            SimulatedAnnealing.LightsTimes: light times of neighbour solution
+        """
         new_light_times = deepcopy(self.lights_times)
         crossroad_id = random.randint(0, 3)
         direction_list = list(Direction)
         add_to = random.choice(direction_list)
         direction_list.remove(add_to)
         remove_from = random.choice(direction_list)
-        print(new_light_times[crossroad_id])
+        if new_light_times[crossroad_id][remove_from] <= 0:
+            return self.neigbour()
         new_light_times[crossroad_id][add_to] += 1
         new_light_times[crossroad_id][remove_from] -= 1
         return new_light_times
 
-    def generate_cycle(self, lights_times, lights_permutation):
+    def generate_cycle(self,
+                       lights_times: LightsTimes,
+                       lights_permutation: LightsPermutation
+                       ) -> List[Crossroad.LightsCycle]:
+        """
+        Generates light cycle based on light times and lights permutation.
+
+        Args:
+            lights_times (LightsTimes)
+            lights_permutation (LightsPermutation)
+
+        Returns:
+            List[Crossroad.LightsCycle]
+        """
         lights_cycle_lst = []
         for crossraod_id in range(4):
             lights_cycle = []
@@ -45,11 +86,17 @@ class SimulatedAnnealing:
             lights_cycle_lst.append(lights_cycle)
         return lights_cycle_lst
 
-    def run(self):
+    def run(self) -> List[Crossroad.LightsCycle]:
+        """
+        Runs simulated Annealing and returns best solution
+
+        Returns:
+            List[Crossroad.LightsCycle]: _description_
+        """
         lowest_score = self.fitness(self.lights_cycle)
-        temperature = 2
-        alfa = 0.95
-        while temperature > 0.001:
+        temperature = 1
+        alfa = 0.996
+        while temperature > 0.0001:
             new_lights_times = self.neigbour()
             new_lights_cycle = self.generate_cycle(
                 new_lights_times, self.lights_permutation)
@@ -62,9 +109,9 @@ class SimulatedAnnealing:
                 self.lights_cycle = new_lights_cycle
                 self.lights_times = new_lights_times
 
-            print(difference)
-            print(self.lights_times)
             temperature *= alfa
+        print(self.lights_times)
+        return self.lights_cycle
 
 
 if __name__ == "__main__":
--- scripts/simulation/simulation.py
+++ scripts/simulation/simulation.py
@@ -29,6 +29,7 @@ type Location = Tuple[int, Direction]
 
 
 class Car:
+    # hardcoding pathfindig
     dest_to_path_map = {(0, 1): [Direction.EAST],
                         (0, 2): [Direction.SOUTH],
                         (0, 3): [Direction.EAST, Direction.NORTH],
@@ -43,12 +44,27 @@ class Car:
                         (3, 2): [Direction.WEST]
                         }
 
-    def __init__(self, origin, destination) -> None:
+    def __init__(self, origin: Location, destination: Location) -> None:
+        """
+        Creates car and its path
+
+        Args:
+            origin (Location): car spawn location
+            destination (Location): car destination
+        """
         self.origin: Location = origin
         self.destination: Location = destination
         self.path: List[Lane] = self.get_path()
+        return
 
-    def get_path(self):
+    def get_path(self) -> List[Direction]:
+        """
+        Finds path for car. Path is represented as directions at consecutive crossroads.
+        If the list is empty car reached its destination
+
+        Returns:
+            List[Direction]: list of directions at crossroads
+        """
         if self.origin[0] == self.destination[0]:
             return [self.destination[1]]
         else:
@@ -66,6 +82,12 @@ class Lane:
         self.processing_counter = 0
 
     def process_cars(self) -> Car:
+        """
+        Process cars in lane if light is green. 
+
+        Returns:
+            Car: car that left the crossroad
+        """
         if not self.queue:
             return None
         if self.processing_counter >= self.processing_time:
@@ -75,23 +97,41 @@ class Lane:
             return None
 
     def add_car(self, car: Car) -> None:
+        """
+        Adds car to the end of queue
+
+        Args:
+            car (Car): car to add
+        """
         self.queue.append(car)
 
 
 class Crossroad:
+    # This is how lanes at crossroad will be represented
     type LaneLocations = Dict[Direction, Lane]
+    type LightsCycle = List[Direction]
 
     def __init__(self, lights_cycle=None) -> None:
-        self.lights_cycle: List[bool] = lights_cycle  # direction (N,E,S,W)
+        # Lights cycle tells witch lane has green light, None=yellow
+        self.lights_cycle: Crossroad.LightsCycle = lights_cycle
         self.in_lanes: Crossroad.LaneLocations = {}
         self.out_lanes: Crossroad.LaneLocations = {}
         self.add_in_lanes()
         self.add_out_lanes()
 
-    def step(self, time) -> int:
+    def step(self, turn: int) -> int:
+        """
+        Runs one simulation step
+
+        Args:
+            time (int): turn in simulation
+
+        Returns:
+            int: score for this step
+        """
         score = 0
-        green_light_now = self.lights_cycle[time]
-        # yellow light so reset all counters
+        green_light_now = self.lights_cycle[turn]
+        # if yellow light reset all counters
         if green_light_now == None:
             self.reset_lights_counters()
         else:
@@ -106,13 +146,25 @@ class Crossroad:
         return score
 
     def reset_lights_counters(self) -> None:
+        """
+        Resets processing counters for all lanes
+        """
         for in_lane in self.in_lanes.values():
             in_lane.processing_counter = 0
 
     def add_car(self, car: 'Car') -> None:
+        """
+        Finds correct lane and appends car to queue.
+
+        Args:
+            car (Car): Car to add
+        """
         self.in_lanes[car.origin[1]].add_car(car)
 
     def add_in_lanes(self) -> None:
+        """
+        Initialize crossroad with lanes going into, can change deepending on crossroad.
+        """
         self.in_lanes[Direction.SOUTH] = Lane([Turn.LEFT, Turn.FORWARD, Turn.RIGHT],
                                               processing_time=2)
         self.in_lanes[Direction.WEST] = Lane([Turn.LEFT, Turn.FORWARD, Turn.RIGHT],
@@ -123,6 +175,9 @@ class Crossroad:
                                              processing_time=2)
 
     def add_out_lanes(self) -> None:
+        """
+        Initialize crossroad with lanes going out, can change deepending on crossroad.
+        """
         self.out_lanes[Direction.SOUTH] = Lane([],
                                                processing_time=2)
         self.out_lanes[Direction.WEST] = Lane([],
@@ -133,23 +188,27 @@ class Crossroad:
                                               processing_time=2)
 
     def reset_queues(self) -> None:
+        """
+        Sets all queues to 0. Used when running new simulation on the same crossroad.
+        """
         for in_lane in self.in_lanes.values():
             in_lane.queue = []
 
 
 class CrossroadNetwork:
     def __init__(self) -> None:
-        self.network_as_graph = {0: [1, 2],
-                                 1: [0, 3],
-                                 2: [0, 3],
-                                 3: [1, 2]
-                                 }
+        """
+        Creates crossroad network represented as list of crossroads.
+        """
         self.crossroad_network: List[Crossroad] = [
             Crossroad(), Crossroad(), Crossroad(), Crossroad()
         ]
         self.connect_crossroads()
 
     def connect_crossroads(self):
+        """
+        Setting some out_lanes of one crossroad to be in_lanes of other.
+        """
         self.crossroad_network[0].out_lanes[Direction.EAST] =\
             self.crossroad_network[1].in_lanes[Direction.WEST]
         self.crossroad_network[0].out_lanes[Direction.SOUTH] =\
@@ -171,39 +230,70 @@ class CrossroadNetwork:
             self.crossroad_network[1].in_lanes[Direction.SOUTH]
 
     def add_car(self, car: Car):
+        """
+        Finds correct crossroad and adds car there.
+
+        Args:
+            car (Car): Car to add
+        """
         self.crossroad_network[car.origin[0]].add_car(car)
 
 
 class Simulation:
     def __init__(self, turn_time, cycles=0) -> None:
         self.crossroad_network = CrossroadNetwork()
-
         self.turn_time = turn_time
         self.cycles = cycles
 
     def run(self) -> int:
+        """
+        Runs simulation and returns score.
+
+        Returns:
+            int: score
+        """
         score = 0
         for _ in range(self.cycles):
             for t in range(self.turn_time):
+                self.add_car()
                 for crossroad in self.crossroad_network.crossroad_network:
-                    self.add_car(t)
                     score += crossroad.step(t)
+
         for crossroad in self.crossroad_network.crossroad_network:
             crossroad.reset_queues()
         return score
 
-    def step(self, t) -> None:
-        self.add_car(t)
-        self.crossroad.step(t)
+    def step(self, t: int) -> None:
+        """
+        Makes step in simulation. Used in graphic representation.
 
-    def add_car(self, car: Car):
+        Args:
+            t (int): Turn in simulation
+        """
+        self.add_car()
+        for crossroad in self.crossroad_network.crossroad_network:
+            crossroad.step(t)
+
+    def add_car(self):
+        """
+        Generates cars from outside world.
+        """
         possible_origins = [(0, Direction.WEST), (0, Direction.NORTH),
                             (1, Direction.EAST), (1, Direction.NORTH),
                             (2, Direction.WEST), (2, Direction.SOUTH),
                             (3, Direction.EAST), (3, Direction.SOUTH),]
-        car_origin = random.choice(possible_origins[:5])
-        car_destination = random.choice(possible_origins)
-        # location = self.car_add_from[t]
+        car_origin = random.choices(possible_origins,
+                                    weights=[4, 1,
+                                             1, 1,
+                                             1, 1,
+                                             1, 1],
+                                    k=1)[0]
+        car_destination = random.choices(possible_origins,
+                                         weights=[1, 1,
+                                                  2, 1,
+                                                  1, 3,
+                                                  1, 1],
+                                         k=1)[0]
         car_to_add = Car(car_origin, car_destination)
         self.crossroad_network.crossroad_network[car_origin[0]].\
             in_lanes[car_origin[1]].add_car(car_to_add)
